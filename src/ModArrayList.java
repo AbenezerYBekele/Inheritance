72c3898dcb7e10701318bc0f39d80bf0dd9e917d
Abenezer Y Bekele <113382918+AbenezerYBekele@users.noreply.github.com>
2025-01-31 22:43:55 -0600

    Update ModArrayList.java

--- src/ModArrayList.java
+++ src/ModArrayList.java
@@ -2,8 +2,11 @@ import java.util.ArrayList;
 
 public class ModArrayList<D> extends ArrayList<D> {
 
-    public D getUsingMod(int Num) {
-        int ValidNum = Math.abs(Num) % this.size();
+    public D getUsingMod(int num) {
+        if (this.isEmpty()) {
+        throw new IndexOutOfBoundsException("Cannot get an element from an empty list");
+        }
+        int ValidNum =  (num % this.size() + this.size()) % this.size(); 
         return  this.get(ValidNum);
     }
 }
