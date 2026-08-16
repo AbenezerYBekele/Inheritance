cc9efa2f9734ba3ad28468b12de11d7dfa7b0dc2
Abenezer Y Bekele <113382918+AbenezerYBekele@users.noreply.github.com>
2026-08-15 22:12:08 -0500

    Delete 1

--- src/ModArrayList.java
+++ src/ModArrayList.java
@@ -6,7 +6,7 @@ public class ModArrayList<D> extends ArrayList<D> {
         if (this.isEmpty()) {
         throw new IndexOutOfBoundsException("Cannot get an element from an empty list");
         }
-        int ValidNum =  (num % this.size() + this.size()) % this.size(); 
+        int ValidNum =  (num % this.size() + this.size()) % this.size();
         return  this.get(ValidNum);
     }
 }
